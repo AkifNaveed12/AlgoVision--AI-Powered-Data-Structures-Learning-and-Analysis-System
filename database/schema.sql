28307bfbef938ce0d39cd3396ab5ca22bf44cb35
AkifNaveed12 <akif27031@gmail.com>
2026-03-05 17:08:50 +0500

    Initial project setup with folder and README

---
+++ database/schema.sql
@@ -0,0 +1,4 @@
+Users (id, email, password)
+Practice_Problems(id, title, description, difficulty, solution)
+Algorithm_Runs(id, user_id, algorithm, input_size, execution_time, memory)
+Reports(id, user_id, report_file)
